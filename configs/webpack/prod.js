25866af50cf2a231760e300d475b292d1008f5dc
dnnzz <denizfiratt03@gmail.com>
2022-08-11 14:04:25 +0300

    webpack config

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,6 +1,7 @@
 // production config
 const { merge } = require("webpack-merge");
 const { resolve } = require("path");
+const webpack = require("webpack");
 
 const commonConfig = require("./common");
 
@@ -12,5 +13,9 @@ module.exports = merge(commonConfig, {
     path: resolve(__dirname, "../../dist"),
     publicPath: "/"
   },
-  plugins: []
+  plugins: [new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }
+  })]
 });
